8733383ae8bf3580fe647f4f12dc2d423ac5b040
aselwilliams <aselwilliams22@gmail.com>
2022-06-07 17:18:55 -0400

    header is added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import Header from './components/Header'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-  
+    <div className="main-container">
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header class="title">
+    <h1>Our Reviews</h1>
+    <div class='underline'></div>
+</header>
+  )
+}
+
+export default Header
